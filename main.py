a7511dda792d5a4a8451c10e4a02662c3a63274e
Firefly-ts <firefly-ts@yandex.ru>
2023-11-02 12:17:06 +0300

    New day - new print

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
     print('My first git program')
     print('First commit')
-
+    print('74eff67cf486cb686977189067ab4b0850ac3439')
+    print('And I change it every day')
 
 if __name__ == '__main__':
     main()
