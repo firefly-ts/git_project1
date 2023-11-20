54dcd7adaabac80ee4b746e645db02b004b4ca83
Firefly-ts <firefly-ts@yandex.ru>
2023-11-20 13:45:30 +0300

    New day 20.11_1

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ def main():
     print('First commit')
     print('74eff67cf486cb686977189067ab4b0850ac3439')
     print('And I change it every day')
-    print("121212")
-
+    print("1212")
 if __name__ == '__main__':
     main()
